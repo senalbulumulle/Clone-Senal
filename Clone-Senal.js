43cd07c21299528dd279c88839668ddf01ef33b2
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2021-02-05 18:31:11 -0500

    Add files via upload

---
+++ Clone-Senal.js
@@ -0,0 +1,38 @@
+/* Clone-Senal.js */
+/* Written by Senal Bulumulle */
+function senal() {
+    document.write("<img src='./Image/senal.png' width='36'></img>"); 
+}
+
+
+function clone() {
+    setInterval(senal, 2);
+    console.log(clone);
+}
+
+
+function cloneConsole() {
+
+    document.write("<img src='./Image/space.png' width='36'></img>");
+}
+
+
+function cloneTimer() {
+    setInterval(cloneConsole, 100);
+}
+
+
+// Call the variables 
+senal();
+clone();
+cloneConsole();
+cloneTimer();
+
+
+// Make a variable or constant called scroll to automatically scroll down. 
+// Once all Senal's are reached to the bottom to the screen, it will start
+// automatically scroll down. 
+const scroll = setInterval(function () { window.scrollBy(0, 3000); }, 0);
+
+// Get a "new" date
+new Date();
